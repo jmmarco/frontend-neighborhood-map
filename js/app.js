c94b63021dd0294a5300ac9e4ca3910ba08e2927
Juan Martin Marco <jmmarco@gmail.com>
2015-09-23 19:31:29 -0300

    Add JS folder and main file

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// Main JS Code
+
+// Create the Map ^_^
+
+var initMap = function() {
+	var map;
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8
+	});
+};
