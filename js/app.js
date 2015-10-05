8a4241842e1b551fd54adfbd36439deb0d706bb3
Juan Martin Marco <jmmarco@gmail.com>
2015-10-05 11:59:36 -0300

    Add geocode for Google Maps

--- js/app.js
+++ js/app.js
@@ -1,17 +1,13 @@
 // Store a couple of locations statically
 var locations = [
 {
-	name: 'coffee code',
-	address: 'Monroe 2211'
+	name: 'Comedy Cellar',
+	address: '117 Macdougal St'
 },
 {
-	name: 'pizza palace',
-	address: 'Olazabal 3001'
-},
-{
-	name: 'ice cream heaven',
-	address: 'Cabildo 2800'
-},
+	name: 'Carolines on Broadway',
+	address: '1626 Broadway'
+}
 
 ];
 
@@ -22,11 +18,7 @@ var locations = [
 
 // Write the View Model
 var viewModel = function() {
-	var self = this;
 
-	this.name = ko.observable('CafeCode');
-	this.address = ko.observable('Av. Cabildo 3000');
-	this.locations = ko.observableArray(locations);
 
 };
 
@@ -44,15 +36,66 @@ ko.applyBindings(new viewModel());
 // Create the Map ^_^ and add map markers
 
 var initMap = function() {
-
+	var geocoder;
 	var map;
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.6158238, lng: -58.4333203},
-		zoom: 12
-	});
+	var address = "New York";
+	geocoder = new google.maps.Geocoder();
+	var latlng = new google.maps.LatLng(40.7033127, -73.979681);
+	var myOptions = {
+		zoom: 7,
+		center: latlng,
+		mapTypeControl: true,
+		mapTypeControlOptions: {style: google.maps.mapTypeControlStyle.DROPDOWN_MENU},
+		navigationControl: true,
+		mapTypeID: google.maps.MapTypeId.ROADMAP
+	};
 
-	google.maps.event.addListener(map, 'bounds_changed', function() {
-    var bounds = map.getBounds();
-	});
+
+	map = new google.maps.Map(document.getElementById('map'), myOptions);
+	if (geocoder) {
+		geocoder.geocode( { 'address': address}, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				if (status != google.maps.GeocoderStatus.ZERO_RESULTS) {
+					map.setCenter(results[0].geometry.location);
+
+					var infowindow = new google.maps.InfoWindow({
+						content: '<b>' + address + '</b>',
+						size: new google.maps.Size(150, 50)
+					});
+
+					var marker = new google.maps.Marker({
+						position: results[0].geometry.location,
+						map: map,
+						title: address
+					});
+
+					google.maps.event.addListener(marker, 'click', function() {
+						infowindow.open(map, marker);
+					});
+
+				} else {
+					alert("No results found");
+				}
+			} else {
+				alert("Geocode was not successful for the following: " + status);
+			}
+		});
+	}
 };
 
+
+
+
+	var marker = new google.maps.Marker({
+		position: map.center,
+		map: map,
+		animation: google.maps.Animation.DROP
+	});
+	console.log(map.center);
+
+	google.maps.event.addListener(map, 'bounds_changed', function() {
+		var bounds = map.getBounds();
+	});
+
+// I called initMap from the <script> tag in index.html, should I call it from within the viewModel instead?
+};
