a084d5a8fbc2e7c284f80d66491fb723fae09bb3
Juan Martin Marco <jmmarco@gmail.com>
2015-10-11 21:05:53 -0300

    Remove search service for Google Maps

--- js/app.js
+++ js/app.js
@@ -1,20 +1,14 @@
 // Store a couple of locations statically
 var locations = [
 {
-	name: 'coffee code',
-	address: 'Monroe 2211'
+	name: 'Comedy Cellar',
+	address: { lat: 40.730219, lng:-74.0005736 }
 },
 {
-	name: 'pizza palace',
-	address: 'Olazabal 3001'
+	name: 'Carolines on Brodway',
+	address: { lat: 40.7610514, lng: -73.9840729 }
 },
-{
-	name: 'ice cream heaven',
-	address: 'Cabildo 2800'
-},
-
 ];
-
 
 
 
@@ -22,11 +16,23 @@ var locations = [
 
 // Write the View Model
 var viewModel = function() {
+	'use strict';
 	var self = this;
-
-	this.name = ko.observable('CafeCode');
-	this.address = ko.observable('Av. Cabildo 3000');
+	var markersArray = [];
 	this.locations = ko.observableArray(locations);
+	console.log(locations.slice(0));
+	self.query = ko.observable('');
+	self.search = function(value) {
+		// remove all locations from the view
+		self.locations.removeAll();
+		for (var i in locations) {
+			console.log(locations[0]);
+			if (locations[i].name().toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+				self.locations().push(locations[i]);
+				console.dir(self.people);
+			}
+		}
+	};
 
 };
 
@@ -35,24 +41,26 @@ ko.applyBindings(new viewModel());
 
 
 
-
-
 // Put data into an observable array
 
 // Wire data to the view
 
-// Create the Map ^_^ and add map markers
-
+// Create the Map ^_^
 var initMap = function() {
+	'use strict';
 
 	var map;
+	var infowindow;
+
+	// Asign the specific lat and lon of NYC to newYorkCity
+	var newYorkCity = new google.maps.LatLng(40.7033127,-73.979681);
+
+	// Create the map and attach it to the ID 'map'
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: -34.6158238, lng: -58.4333203},
+		center: newYorkCity,
 		zoom: 12
 	});
 
-	google.maps.event.addListener(map, 'bounds_changed', function() {
-    var bounds = map.getBounds();
-	});
+	// Assign the function InfoWindow to infowindow
+	infowindow = new google.maps.InfoWindow();
 };
-
