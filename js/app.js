e4a29a858e1725c7aeefa973325e4e024951a51d
Juan Martin Marco <jmmarco@gmail.com>
2015-10-15 23:14:16 -0300

    Specify lat and lng separately

--- js/app.js
+++ js/app.js
@@ -1,53 +1,63 @@
-// Store a couple of locations statically using latittude and longitude as address
+// Locations
 var locations = [
 {
 	name: 'Comedy Cellar',
-	address: { lat: 40.730219, lng:-74.0005736 },
+	lat: 40.730219,
+	lng: -74.0005736,
 	marker: "A"
 },
 {
 	name: 'Carolines on Brodway',
-	address: { lat: 40.7610514, lng: -73.9840729 },
+	lat: 40.7610514,
+	lng: -73.9840729,
 	marker: "B"
 },
 {
 	name: 'Ben\'s Pizzeria',
-	address: { lat: 40.730636, lng: -74.000335 },
+	lat: 40.730636,
+	lng: -74.000335,
 	marker: "C"
 },
 {
 	name: 'Billy\'s Bakery',
-	address: { lat: 40.745533, lng: -74.001901 },
+	lat: 40.745533,
+	lng: -74.001901,
 	marker: "D"
 },
 {
 	name: 'Asuka Sushi',
-	address: { lat: 40.7452362, lng: -74.0011433 },
+	lat: 40.7452362,
+	lng: -74.0011433,
 	marker: "E"
 },
 {
 	name: 'Joe\'s Pizzeria',
-	address: { lat: 40.7305589, lng: -74.0026389 },
+	lat: 40.7305589,
+	lng: -74.0026389,
 	marker: "F"
 },
 {
 	name: 'Bryant Park',
-	address: { lat: 40.7536005, lng: -73.9854266 },
+	lat: 40.7536005,
+	lng: -73.9854266,
 	marker: "G"
 },
 {
 	name: 'Central Park',
-	address: { lat: 40.7828687, lng: -73.9675491 },
+	lat: 40.7828687,
+	lng: -73.9675491,
 	marker: "H"
 },
 {
 	name: 'Prospect Park',
-	address: { lat: 40.6805138, lng: -73.9609889 },
+	lat: 40.6805138,
+	lng: -73.9609889,
 	marker: "I"
 },
 {
 	name: 'Wythe Hotel',
-	address: { lat: 40.7203352, lng: -73.9575497 },
+	lat: 40.7203352,
+	lng: -73.9575497,
 	marker: "J"
 }
 ];
@@ -57,15 +67,13 @@ var locations = [
 
 // Write the View Model
 var viewModel = function() {
-	'use strict';
 	var self = this;
-	var markersArray = [];
 	self.locations = ko.observableArray(locations); // make the locations an obs array
 
 	self.query = ko.observable('');
 
 
-	//self.locations.removeAll();
+	self.locations.removeAll(this);
 	self.search = ko.computed(function() {
 		return ko.utils.arrayFilter(self.locations(), function(location) {
 			return location.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
@@ -79,33 +87,39 @@ ko.applyBindings(new viewModel());
 
 
 
-// Put data into an observable array
-
-// Wire data to the view
-
-// Define map variables outside of function for easy access
+// Map Stuff ^_^
 var map;
 var infowindow;
-var marker;
+var markersArray = [];
 
-// Create the Map ^_^
 var initMap = function() {
-	'use strict';
 
-	// Asign the specific lat and lon of NYC to newYorkCity
-	var newYorkCity = new google.maps.LatLng(40.7033127,-73.979681);
+	// Asign the specific lat and lng of NYC
+	var newYorkCity = {lat: 40.7033127, lng: -73.979681};
 
-	// Create the map and attach it to the ID 'map'
 	map = new google.maps.Map(document.getElementById('map'), {
+		// Map Options
 		center: newYorkCity,
 		fitbounds: true,
 		zoom: 12
 	});
 
-	// Assign the function InfoWindow to infowindow
+	// Info Windows
 	infowindow = new google.maps.InfoWindow();
 
-	marker = new google.maps.Marker({
+
+	// push markers into the markers array
+	for (var i = 0; i < locations.length; i++) {
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[0].lat, locations[0].lng),
+			title: '<p>' + locations[0].name + '</p>',
+			map: map
+		});
+	}
+
+	markersArray.push(marker);
+
+	/*marker = new google.maps.Marker({
 	map: map,
 	draggable: true,
 	animation: google.maps.Animation.DROP,
@@ -118,5 +132,5 @@ var initMap = function() {
 		} else {
 			marker.setAnimation(google.maps.Animation.BOUNCE);
 		}
-	}
+	}*/
 };
