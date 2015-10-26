3b74468e5c6fb73d4dbd8d06b87f50b52a23ebb6
Juan Martin Marco <jmmarco@gmail.com>
2015-10-25 21:42:12 -0300

    Add accurate coordinates for each location

--- js/app.js
+++ js/app.js
@@ -1,73 +1,74 @@
-// Locations
-// LatLng object literals are not supported in the Geometry library (Google Maps). Specfied them separately
+// ----------- Locations ----------- //
 var locationData = [
 {
 	name: 'Comedy Cellar',
-	lat: 40.730219,
-	lng: -74.0027623,
+	lat: 40.730219 ,
+	lng: -74.00057400000003,
 	marker: null
 },
 {
 	name: 'Carolines on Brodway',
-	lat: 40.7610514,
-	lng: -73.9862616,
+	lat: 40.761051 ,
+	lng: -73.98407299999997,
 	marker: null
 },
 {
 	name: 'Ben\'s Pizzeria',
-	lat: 40.7272897,
-	lng: -74.0012167,
+	lat: 40.730392 ,
+	lng: -74.00032399999998,
 	marker: null
 },
 {
 	name: 'Billy\'s Bakery',
-	lat: 40.7453376,
-	lng: -74.0040787,
+	lat: 40.717715 ,
+	lng: -74.00447600000001,
 	marker: null
 },
 {
 	name: 'Asuka Sushi',
-	lat: 40.7452322,
-	lng: -74.001138,
+	lat: 40.745232 ,
+	lng: -73.99894899999998,
 	marker: null
 },
 {
 	name: 'Joe\'s Pizza',
-	lat: 40.730559,
-	lng: -74.004362,
+	lat: 40.730559 ,
+	lng: -74.00216799999998,
 	marker: null
 },
 {
 	name: 'Bryant Park',
-	lat: 40.7535965,
-	lng: -73.9854213,
+	lat: 40.753596 ,
+	lng:  -73.98323299999998,
 	marker: null
 },
 {
 	name: 'Central Park',
-	lat: 40.7828647,
-	lng: -73.9675438,
+	lat: 40.782865 ,
+	lng: -73.96535499999999,
 	marker: null
 },
 {
 	name: 'Prospect Park',
-	lat: 40.6510946,
-	lng: -73.981447,
+	lat: 40.660204 ,
+	lng: -73.96895599999999,
 	marker: null
 },
 {
 	name: 'Wythe Hotel',
-	lat: 40.7219936,
-	lng: -73.9601151,
+	lat: 40.721994,
+	lng: -73.95792599999999,
 	marker: null
 }
 ];
 
 
-// Write the View Model
+// ----------- View Model ----------- //
 var viewModel = function() {
 	var self = this;
 	self.locations = ko.observableArray(locationData);
+
+	self.markers = ko.observableArray(markersArray);
 
 	self.query = ko.observable('');
 
@@ -78,7 +79,7 @@ var viewModel = function() {
 			console.log("Evaluating Search..");
 			return ko.utils.arrayFilter(self.locations(), function(location) {
 				return location.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
-				});
+			});
 		}
 	});
 };
@@ -88,31 +89,45 @@ ko.applyBindings(new viewModel());
 
 
 
-// Map Stuff ^_^
+// ----------- Map ----------- //
 var map;
 var infowindow;
 var markersArray = [];
 var marker;
+var mapOptions;
 
 var initMap = function() {
 
 	// Asign the specific lat and lng of NYC
-	var newYorkCity = {lat: 40.7080936, lng: -73.9941321};
+	var newYorkCity = {lat: 40.7127, lng: -74.0059};
 
-	map = new google.maps.Map(document.getElementById('map'), {
-		// Map Options
+	// Map Options
+	mapOptions = {
 		center: newYorkCity,
-		fitbounds: false,
-		zoom: 12
-	});
+		fitbounds: true,
+		zoom: 13,
+	};
 
-	// Info Windows
-	infowindow = new google.maps.InfoWindow();
+	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+	var marker = new google.maps.Marker({
+		position: newYorkCity,
+    	map: map,
+    	animation: google.maps.Animation.DROP,
+    	title: 'Hello World!'
+  	});
+
+  	// To remove a marker uncomment line below
+  	// marker.setMap(null);
 
 
-	//var contentString = '<div>' + marker.name + '</div>';
+
+
+
 
 	// push markers into the markers array
+	//var bounds = new google.maps.LatLngBounds();
+
 	for (var i = 0; i < locationData.length; i++) {
 		marker = new google.maps.Marker({
 			position: new google.maps.LatLng(locationData[i].lat, locationData[i].lng),
@@ -122,20 +137,26 @@ var initMap = function() {
 			map: map
 		});
 		markersArray.push(marker);
+		marker.addListener('click', toggleBounce);
 	}
 
 
-	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.open(map, marker);
-	});
-
-	/*
-	marker.addListener('click', toggleBounce);
 	function toggleBounce() {
 		if (marker.getAnimation() !== null) {
-		marker.setAnimation(null);
+			marker.setAnimation(null);
 		} else {
 			marker.setAnimation(google.maps.Animation.BOUNCE);
 		}
-	}*/
+	}
+
+
+	/*google.maps.event.addListener(marker, 'click', function() {
+		infowindow.open(map, marker);
+	});*/
+
+
+
 };
+
+// ----------- AJAX ----------- //
+
