c1f5143580c94adbbefd964466feb1c06c750ca6
Juan Martin Marco <jmmarco@gmail.com>
2015-11-04 21:13:38 -0300

    Added infowindows

--- js/app.js
+++ js/app.js
@@ -1,148 +1,161 @@
 // ----------- Locations ----------- //
-var locationData = [
-{
-	name: 'Comedy Cellar',
-	lat: 40.730219,
-	lng: -74.00057400000003,
-	marker: null
-},
-{
-	name: 'Carolines on Brodway',
-	lat: 40.761051,
-	lng: -73.98407299999997,
-	marker: null
-},
-{
-	name: 'Ben\'s Pizzeria',
-	lat: 40.730392,
-	lng: -74.00032399999998,
-	marker: null
-},
-{
-	name: 'Billy\'s Bakery',
-	lat: 40.717715,
-	lng: -74.00447600000001,
-	marker: null
-},
-{
-	name: 'Asuka Sushi',
-	lat: 40.745232,
-	lng: -73.99894899999998,
-	marker: null
-},
-{
-	name: 'Joe\'s Pizza',
-	lat: 40.730559,
-	lng: -74.00216799999998,
-	marker: null
-},
-{
-	name: 'Bryant Park',
-	lat: 40.753596,
-	lng:  -73.98323299999998,
-	marker: null
-},
-{
-	name: 'Central Park',
-	lat: 40.782865,
-	lng: -73.96535499999999,
-	marker: null
-},
-{
-	name: 'Prospect Park',
-	lat: 40.660204,
-	lng: -73.96895599999999,
-	marker: null
-},
-{
-	name: 'Wythe Hotel',
-	lat: 40.721994,
-	lng: -73.95792599999999,
-	marker: null
-}
-];
+var locationData = [{
+    name: 'Comedy Cellar',
+    lat: 40.730219,
+    lng: -74.00057400000003,
+    marker: null
+}, {
+    name: 'Carolines on Brodway',
+    lat: 40.761051,
+    lng: -73.98407299999997,
+    marker: null
+}, {
+    name: 'Ben\'s Pizzeria',
+    lat: 40.730392,
+    lng: -74.00032399999998,
+    marker: null
+}, {
+    name: 'Billy\'s Bakery',
+    lat: 40.717715,
+    lng: -74.00447600000001,
+    marker: null
+}, {
+    name: 'Asuka Sushi',
+    lat: 40.745232,
+    lng: -73.99894899999998,
+    marker: null
+}, {
+    name: 'Joe\'s Pizza',
+    lat: 40.730559,
+    lng: -74.00216799999998,
+    marker: null
+}, {
+    name: 'Bryant Park',
+    lat: 40.753596,
+    lng: -73.98323299999998,
+    marker: null
+}, {
+    name: 'Central Park',
+    lat: 40.782865,
+    lng: -73.96535499999999,
+    marker: null
+}, {
+    name: 'Prospect Park',
+    lat: 40.660204,
+    lng: -73.96895599999999,
+    marker: null
+}, {
+    name: 'Wythe Hotel',
+    lat: 40.721994,
+    lng: -73.95792599999999,
+    marker: null
+}];
 
 
 // ----------- View Model ----------- //
 var viewModel = function() {
-	var self = this;
+    var self = this;
 
-	mapOptions = {
-		zoom: 12,
-		center: {lat: 40.7127, lng: -74.0059},
-	};
+    mapOptions = {
+        zoom: 12,
+        center: {
+            lat: 40.7127,
+            lng: -74.0059
+        },
+    };
 
-	self.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-
-
-	self.allLocations = [];
-	locationData.forEach(function(venue) {
-		self.allLocations.push(new Venue(venue));
-		//console.log(self.allLocations);
-		});
+    self.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    self
 
 
-	self.allLocations.forEach(function(venue) {
-		var markerOptions = {
-			map: self.map,
-			position: venue.LatLng,
-			animation: google.maps.Animation.DROP,
-			title: '<p>' + locationData.name + '</p>'
-		};
-		// Add event listener
-		venue.marker = new google.maps.Marker(markerOptions);
-		venue.marker.addListener('click', toggleBounce);
 
-		// Add Google's toggle Bounce
-		function toggleBounce() {
-			if (venue.marker.getAnimation() !== null) {
-				venue.marker.setAnimation(null);
-			} else {
-				venue.marker.setAnimation(google.maps.Animation.BOUNCE);
-				setTimeout(function() {
-					venue.marker.setAnimation(null);
-				}, 2000);
-			}
-		}
 
-	});
+    self.allLocations = [];
+    locationData.forEach(function(venue) {
+        self.allLocations.push(new Venue(venue));
+        //console.log(self.allLocations);
+    });
 
-	self.visibleVenues = ko.observableArray();
 
-	self.allLocations.forEach(function(venue) {
-		self.visibleVenues.push(venue);
-	});
+    self.allLocations.forEach(function(venue) {
 
-	self.userInput = ko.observable('');
 
-	self.filterMarkers = function() {
-		var searchInput =  self.userInput().toLowerCase();
-		self.visibleVenues.removeAll();
+        var contentString = '<div>' + venue.name + '</div>';
 
-		self.allLocations.forEach(function(venue) {
-			venue.marker.setVisible(false);
+        venue.infowindow = new google.maps.InfoWindow({
+            position: venue.LatLng,
+            content: contentString
+        });
 
-			if (venue.name.toLowerCase().indexOf(searchInput) !== -1) {
-				self.visibleVenues.push(venue);
-			}
-		});
 
-		self.visibleVenues().forEach(function(venue) {
-			venue.marker.setVisible(true);
-		});
-	};
 
-	function Venue(dataObj) {
-		this.name = dataObj.name;
-		console.log(this.name);
-		this.LatLng = { lat: dataObj.lat, lng: dataObj.lng };
-		console.log(this.LatLng);
 
-		// save  a reference to venue's map marker
+        var markerOptions = {
+            map: self.map,
+            position: venue.LatLng,
+            animation: google.maps.Animation.DROP,
+            title: venue.name
+        };
+        // Add event listener
+        venue.marker = new google.maps.Marker(markerOptions);
+        venue.marker.addListener('click', toggleBounce);
 
-		this.marker = null;
-	}
+        // Add Google's toggle Bounce
+        function toggleBounce() {
+            if (venue.marker.getAnimation() !== null) {
+                venue.marker.setAnimation(null);
+            } else {
+                venue.marker.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function() {
+                    venue.marker.setAnimation(null);
+                    venue.infowindow.setContent(contentString);
+                    venue.infowindow.open(self.map, venue.marker);
+                }, 2000);
+            }
+        }
+
+
+
+    });
+
+    self.visibleVenues = ko.observableArray();
+
+    self.allLocations.forEach(function(venue) {
+        self.visibleVenues.push(venue);
+    });
+
+    self.userInput = ko.observable('');
+
+    self.filterMarkers = function() {
+        var searchInput = self.userInput().toLowerCase();
+        self.visibleVenues.removeAll();
+
+        self.allLocations.forEach(function(venue) {
+            venue.marker.setVisible(false);
+
+            if (venue.name.toLowerCase().indexOf(searchInput) !== -1) {
+                self.visibleVenues.push(venue);
+            }
+        });
+
+        self.visibleVenues().forEach(function(venue) {
+            venue.marker.setVisible(true);
+        });
+    };
+
+    function Venue(dataObj) {
+        this.name = dataObj.name;
+        console.log(this.name);
+        this.LatLng = {
+            lat: dataObj.lat,
+            lng: dataObj.lng
+        };
+        console.log(this.LatLng);
+
+        // save  a reference to venue's map marker
+
+        this.marker = null;
+    }
 
 };
 
@@ -262,13 +275,13 @@ var config = {
 
 // Uber
 var uberClientID = 'cjKN8yPx-XUsLw1Z77bywimofddFMaDQ',
-uberServerToken = 'fOLxOsSYP0wFnCeJE8mV1KZSS5643TNaWuqnuPWT';
+    uberServerToken = 'fOLxOsSYP0wFnCeJE8mV1KZSS5643TNaWuqnuPWT';
 
 
 
 // Store the latitude and longitude
 var userLatitude, userLongitude,
-partyLatitude, partyLongitude;
+    partyLatitude, partyLongitude;
 
 userLongitude = 40.7127;
 userLongitude = -74.0059;
@@ -279,27 +292,27 @@ partyLongitude = locationData[0].lng;
 
 
 
-	//Query UBER API if needed
-	getEstimatesForUserLocation(userLatitude, userLongitude);
+//Query UBER API if needed
+getEstimatesForUserLocation(userLatitude, userLongitude);
 
 
 function getEstimatesForUserLocation(latitude, longitude) {
-	$.ajax({
-		url: "https://api.uber.com/v1/estimates/price",
-		headers: {
-			Authorization: "Token " + uberServerToken
-		},
-		dataType: "jsonp",
-		data: {
-			start_latitude: latitude,
-			start_longitude: longitude,
-			end_latitude: partyLatitude,
-			end_longitude: partyLongitude
-		},
-		success: function(result) {
-			console.log(result);
-		}
-	});
+    $.ajax({
+        url: "https://api.uber.com/v1/estimates/price",
+        headers: {
+            Authorization: "Token " + uberServerToken
+        },
+        dataType: "jsonp",
+        data: {
+            start_latitude: latitude,
+            start_longitude: longitude,
+            end_latitude: partyLatitude,
+            end_longitude: partyLongitude
+        },
+        success: function(result) {
+            console.log(result);
+        }
+    });
 }
 
 /*
