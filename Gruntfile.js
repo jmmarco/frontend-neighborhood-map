12f10dd5a998dd846ed83e2fc7575c01b20ca69d
Juan Martin Marco <jmmarco@gmail.com>
2015-11-20 13:04:23 -0300

    Adjust input and output folders

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,16 +3,18 @@ module.exports = function(grunt) {
     // Project configuration
     grunt.initConfig({
       concat: {
-        basic: {
-          src: ['src/js/app.js'],
-          dest: 'dist/js/app.js',
-        },
-        extras: {
-          src: ['src/bower_components/bootstrap/dist/js/bootstrap.min.js',
+        js: {
+          src: ['src/js/app.js',
+          'src/bower_components/bootstrap/dist/js/bootstrap.min.js',
           'src/bower_components/jquery/dist/jquery.min.js',
           'src/bower_components/knockout/dist/knockout.js'],
-          dest: 'dist/js/scripts.js',
+          dest: 'dist/js/app.js',
         },
+        css: {
+            src: ['src/css/style.css','src/bower_components/bootstrap/dist/css/bootstrap.min.css',
+            'src/bower_components/font-awesome/css/font-awesome.min.css'],
+            dest: 'dist/css/styles.css'
+        }
       },
     });
 
